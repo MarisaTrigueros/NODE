07c866d7b999195099562a870418551241d3965f
Marisa <marisatriguerosperez@gmail.com>
2024-08-22 00:25:11 +0200

    Update

--- sumScript.js
+++ sumScript.js
@@ -1,3 +1,11 @@
 let sum = 5 + 6; 
 
 console.log(`Sum result: ${sum}`);
+
+//! node
+// Welcome to Node.js v21.7.3.
+// Type ".help" for more information.
+//! > let sum = 5 + 6; console.log(`Sum result: ${sum}`);
+//! Sum result: 11
+// undefined
+// >
